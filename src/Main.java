71c67e8824d903e210761a31fe40bc65ff347501
Usachev13 <2893318@gmail.com>
2023-05-22 20:41:34 +0300

    task2 done

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,18 @@ public class Main {
             month += 1;
             System.out.println("Месяц " + month +", сумма накоплений равна " + total + " рублей");
         }
+
+        /* task2 */
+        int num = 1;
+        while (num <= 10){
+            System.out.print(num + " ");
+            num++;
+        }
+        System.out.println();
+        int num2 = 10;
+        while (num2 >= 1){
+            System.out.print(num2 + " ");
+            num2--;
+        }
     }
 }
