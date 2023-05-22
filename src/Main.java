a6f8526fb9bd261def4d5630a3d0e4df87aca520
Usachev13 <2893318@gmail.com>
2023-05-22 20:21:57 +0300

    task1 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hw7 starts");
+        /* task1 */
+        int total = 0;
+        int month = 0;
+        while (total < 2_459_000){
+            total += 15_000;
+            month += 1;
+            System.out.println("Месяц " + month +", сумма накоплений равна " + total + " рублей");
+        }
     }
 }
